ac6561b64878b69ab32b4559983f7983dafd5cee
Fabiana Araujo <fnaraujo85@gmail.com>
2022-04-11 16:29:31 -0300

    resolve a terceira parte dos exemplos

--- aula-4.1.js
+++ aula-4.1.js
@@ -1,3 +1,4 @@
+// 1 parte
 const myName = "Fabiana";
 const birthCity = "Aracaju";
 let birthYear = 1985;
@@ -10,6 +11,7 @@ birthYear = 2030;
 
 console.log(birthYear);
 
+// 2 parte
 let patientId = "50";
 let isEnrolled = true;
 const patientInfo = {
@@ -27,3 +29,17 @@ const area = base * height;
 console.log(base);
 console.log(height);
 console.log(area);
+
+// 3 parte
+const nota = 85;
+if (nota >= 80) {
+    console.log("Parabéns, vovê foi aprovada(o)!");
+}
+if(nota < 80 && nota >= 60) {
+    console.log("Você está na nossa lista de espera");
+}
+if(nota < 60){
+    console.log("Você foi reprovada(o)");
+}else{
+    console.log("Nota invalida");
+}
