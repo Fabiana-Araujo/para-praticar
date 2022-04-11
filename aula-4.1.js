39cb1d34538bd177017de08305a8f9ab9c2cd0c1
Fabiana Araujo <fnaraujo85@gmail.com>
2022-04-11 16:12:56 -0300

    resolve a segunda parte de exemplos

--- aula-4.1.js
+++ aula-4.1.js
@@ -9,3 +9,21 @@ console.log(birthYear);
 birthYear = 2030;
 
 console.log(birthYear);
+
+let patientId = "50";
+let isEnrolled = true;
+const patientInfo = {
+    firstName: "Ana",
+    lastName: "Santos",
+};
+const patientEmail = "ana@email.com";
+
+console.log(typeof patientId);
+
+const base = 5;
+const height = 8;
+const area = base * height;
+
+console.log(base);
+console.log(height);
+console.log(area);
